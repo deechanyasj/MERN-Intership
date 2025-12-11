4cb7be3e2811c23f69bb29180b03917d2bf28dab
Deechanya <sjdeechanya@gmail.com>
2025-12-11 08:59:57 +0530

    Day 6

---
+++ seceFrontend2025Dec/src/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <p>The above content is Navbar</p>
+    </nav>
+  );
+}
+
+export default Navbar;
